a19b42dcc67bd35a23a52313334161d69fc2ae0d
Solid-One <solid.glider@gmail.com>
2026-06-01 19:41:43 +0530

    Remove compiled JS from repo; keep api tsconfig for Vercel

--- api/index.js
+++
@@ -1,3 +0,0 @@
-import { createApp } from "../server/app.js";
-const app = createApp();
-export default app;
--- server/async-route.js
+++
@@ -1,5 +0,0 @@
-export function asyncRoute(handler) {
-    return (req, res, next) => {
-        void handler(req, res).catch(next);
-    };
-}
